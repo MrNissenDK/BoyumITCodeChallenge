242d4e3387c9b569eb50870c484edf2e7e7c9489
Mads Rossen Nissen <mrnissen@live.dk>
2021-02-25 13:26:00 +0100

    Add files via upload

--- BoyumITCodeChallenge/models/WebOrder.cs
+++ BoyumITCodeChallenge/models/WebOrder.cs
@@ -1,1 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
+namespace BoyumITCodeChallenge.models
+{
+    [XmlRoot("WebOrder")]
+    public class WebOrder
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+        [XmlElement("Customer")]
+        public string Customer { get; set; }
+        [XmlElement("Date")]
+        public string _Date {get; set;}
+        [XmlIgnore]
+        public DateTime Date {
+            get { return DateTime.ParseExact(_Date, "yyyyMMdd", null); }
+        }
+        [XmlArray("Items")]
+        public List<WebOrderItem> Items { get; set; }
+
+        [XmlIgnore]
+        public decimal totalPrice {
+            get {
+                decimal sum = 0;
+                foreach( WebOrderItem item in Items)
+                {
+                    sum += item.Price * item.Quantity;
+                }
+                return sum;
+            }
+        }
+        [XmlIgnore]
+        public decimal priceAverage
+        {
+            get
+            {
+                decimal sum = 0;
+                decimal totalItems = 0;
+                foreach (WebOrderItem item in Items)
+                {
+                    sum += item.Price * item.Quantity;
+                    totalItems += item.Quantity;
+                }
+                if (totalItems == 0)
+                    return 0;
+                return sum / totalItems;
+            }
+        }
+    }
+}
---
+++ BoyumITCodeChallenge/models/WebOrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace BoyumITCodeChallenge.models
+{
+    [XmlType("Item")]
+    public class WebOrderItem
+    {
+        [XmlAttribute("description")]
+        public string Description { get; set; }
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+        [XmlElement("Quantity")]
+        public decimal Quantity { get; set; }
+    }
+}
